9fa806813b1b636c1793c1b7a50651187ba34dec
Priyanshu Tiwari <tpriyanshu90@gmail.com>
2018-11-19 02:52:51 +0530

    done

--- app.py
+++ app.py
@@ -158,4 +158,4 @@ def send_message(recipient_id, response):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', port=port)
